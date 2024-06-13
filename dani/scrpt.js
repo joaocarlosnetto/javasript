75d47d90cb3a1ee5f7efafb4ed9d77a36d7bdc9b
joão carlos de caravlho <140458265+joaocarlosnetto@users.noreply.github.com>
2024-06-13 19:20:16 -0300

    editei meus projetos deste repositório

--- dani/scrpt.js
+++ dani/scrpt.js
@@ -1,52 +1,302 @@
+
 function debut(){
-    var d = document.getElementById('debut')
-    d.style.width = '397px'
-    d.style.transition = '0.5s'
+var d = document.getElementById('debut')
+    d.style.width = '380px'
+    d.style.transition = '0.4s'
     var f = document.getElementById('fearless')
-    f.style.width = '90px'
+    f.style.width = '98px'
     var s = document.getElementById('spn')
-    s.style.width = '90px'
+    s.style.width = '98px'
     var r = document.getElementById('red')
-    r.style.width = '90px'
+    r.style.width = '98px'
     var n = document.getElementById('nine')
-    n.style.width = '90px'
+    n.style.width = '98px'
     var r = document.getElementById('rep')
-    r.style.width = '90px'
+    r.style.width = '98px'
     var l = document.getElementById('lover')
-    l.style.width = '90px'
+    l.style.width = '98px'
     var fk = document.getElementById('folk')
-    fk.style.width = '90px'
+    fk.style.width = '98px'
     var e = document.getElementById('ever')
-    e.style.width = '90px'
+    e.style.width = '98px'
     var m = document.getElementById('mid')
-    m.style.width = '90px'
+    m.style.width = '98px'
     var t = document.getElementById('ttpd')
-    t.style.width = '90px'
+    t.style.width = '98px'
+}
+
+ 
+function fearless(){
+    var f = document.getElementById('fearless')
+    f.style.width = '380px'
+    f.style.transition = '0.4s'
+    var d = document.getElementById('debut')
+    d.style.width = '98px'
+    var s = document.getElementById('spn')
+    s.style.width = '98px'
+    var r = document.getElementById('red')
+    r.style.width = '98px'
+    var n = document.getElementById('nine')
+    n.style.width = '98px'
+    var r = document.getElementById('rep')
+    r.style.width = '98px'
+    var l = document.getElementById('lover')
+    l.style.width = '98px'
+    var fk = document.getElementById('folk')
+    fk.style.width = '98px'
+    var e = document.getElementById('ever')
+    e.style.width = '98px'
+    var m = document.getElementById('mid')
+    m.style.width = '98px'
+    var t = document.getElementById('ttpd')
+    t.style.width = '98px'
     
 }
 
-function fearless(){
-    var d = document.getElementById('fearless')
-    d.style.width = '397px'
-    var f = document.getElementById('debut')
-    f.style.width = '90px'
+
+function spn(){
     var s = document.getElementById('spn')
-    s.style.width = '90px'
+    s.style.width = '380px'
+    s.style.transition = '0.4s'
+    var d = document.getElementById('debut')
+    d.style.width = '98px'
+    var f = document.getElementById('fearless')
+    f.style.width = '98px'
     var r = document.getElementById('red')
-    r.style.width = '90px'
+    r.style.width = '98px'
     var n = document.getElementById('nine')
-    n.style.width = '90px'
+    n.style.width = '98px'
     var r = document.getElementById('rep')
-    r.style.width = '90px'
+    r.style.width = '98px'
     var l = document.getElementById('lover')
-    l.style.width = '90px'
+    l.style.width = '98px'
     var fk = document.getElementById('folk')
-    fk.style.width = '90px'
+    fk.style.width = '98px'
     var e = document.getElementById('ever')
-    e.style.width = '90px'
+    e.style.width = '98px'
     var m = document.getElementById('mid')
-    m.style.width = '90px'
+    m.style.width = '98px'
     var t = document.getElementById('ttpd')
-    t.style.width = '90px'
+    t.style.width = '98px'
+    
+}
+
+function red(){
+    var r = document.getElementById('red')
+    r.style.width = '380px'
+    r.style.transition = '0.4s'
+    var d = document.getElementById('debut')
+    d.style.width = '98px'
+    var s = document.getElementById('spn')
+    s.style.width = '98px'
+    var f = document.getElementById('fearless')
+    f.style.width = '98px'
+    var n = document.getElementById('nine')
+    n.style.width = '98px'
+    var r = document.getElementById('rep')
+    r.style.width = '98px'
+    var l = document.getElementById('lover')
+    l.style.width = '98px'
+    var fk = document.getElementById('folk')
+    fk.style.width = '98px'
+    var e = document.getElementById('ever')
+    e.style.width = '98px'
+    var m = document.getElementById('mid')
+    m.style.width = '98px'
+    var t = document.getElementById('ttpd')
+    t.style.width = '98px'
+    
+}
+
+
+function nine(){
+    var n = document.getElementById('nine')
+    n.style.width = '380px'
+    n.style.transition = '0.4s'
+    var d = document.getElementById('debut')
+    d.style.width = '98px'
+    var s = document.getElementById('spn')
+    s.style.width = '98px'
+    var f = document.getElementById('fearless')
+    f.style.width = '98px'
+    var r = document.getElementById('red')
+    r.style.width = '98px'
+    var r = document.getElementById('rep')
+    r.style.width = '98px'
+    var l = document.getElementById('lover')
+    l.style.width = '98px'
+    var fk = document.getElementById('folk')
+    fk.style.width = '98px'
+    var e = document.getElementById('ever')
+    e.style.width = '98px'
+    var m = document.getElementById('mid')
+    m.style.width = '98px'
+    var t = document.getElementById('ttpd')
+    t.style.width = '98px'
+    
+}
+
+
+function rep(){
+    var r = document.getElementById('rep')
+    r.style.width = '380px'
+    r.style.transition = '0.4s'
+    var d = document.getElementById('debut')
+    d.style.width = '98px'
+    var s = document.getElementById('spn')
+    s.style.width = '98px'
+    var f = document.getElementById('fearless')
+    f.style.width = '98px'
+    var n = document.getElementById('nine')
+    n.style.width = '98px'
+    var r = document.getElementById('red')
+    r.style.width = '98px'
+    var l = document.getElementById('lover')
+    l.style.width = '98px'
+    var fk = document.getElementById('folk')
+    fk.style.width = '98px'
+    var e = document.getElementById('ever')
+    e.style.width = '98px'
+    var m = document.getElementById('mid')
+    m.style.width = '98px'
+    var t = document.getElementById('ttpd')
+    t.style.width = '98px'
+    
+}
+
+
+function lover(){
+    var l = document.getElementById('lover')
+    l.style.width = '380px'
+    l.style.transition = '0.4s'
+    var d = document.getElementById('debut')
+    d.style.width = '98px'
+    var s = document.getElementById('spn')
+    s.style.width = '98px'
+    var f = document.getElementById('fearless')
+    f.style.width = '98px'
+    var n = document.getElementById('nine')
+    n.style.width = '98px'
+    var r = document.getElementById('rep')
+    r.style.width = '98px'
+    var r = document.getElementById('red')
+    r.style.width = '98px'
+    var fk = document.getElementById('folk')
+    fk.style.width = '98px'
+    var e = document.getElementById('ever')
+    e.style.width = '98px'
+    var m = document.getElementById('mid')
+    m.style.width = '98px'
+    var t = document.getElementById('ttpd')
+    t.style.width = '98px'
+    
+}
+
+
+function folk(){
+    var fk = document.getElementById('folk')
+    fk.style.width = '380px'
+    fk.style.transition = '0.4s'
+    var d = document.getElementById('debut')
+    d.style.width = '98px'
+    var s = document.getElementById('spn')
+    s.style.width = '98px'
+    var f = document.getElementById('fearless')
+    f.style.width = '98px'
+    var n = document.getElementById('nine')
+    n.style.width = '98px'
+    var r = document.getElementById('rep')
+    r.style.width = '98px'
+    var l = document.getElementById('lover')
+    l.style.width = '98px'
+    var r = document.getElementById('red')
+    r.style.width = '98px'
+    var e = document.getElementById('ever')
+    e.style.width = '98px'
+    var m = document.getElementById('mid')
+    m.style.width = '98px'
+    var t = document.getElementById('ttpd')
+    t.style.width = '98px'
+    
+}
+
+function ever(){
+    var r = document.getElementById('ever')
+    r.style.width = '380px'
+    r.style.transition = '0.4s'
+    var d = document.getElementById('debut')
+    d.style.width = '98px'
+    var s = document.getElementById('spn')
+    s.style.width = '98px'
+    var f = document.getElementById('fearless')
+    f.style.width = '98px'
+    var n = document.getElementById('nine')
+    n.style.width = '98px'
+    var r = document.getElementById('rep')
+    r.style.width = '98px'
+    var l = document.getElementById('lover')
+    l.style.width = '98px'
+    var fk = document.getElementById('red')
+    fk.style.width = '98px'
+    var e = document.getElementById('folk')
+    e.style.width = '98px'
+    var m = document.getElementById('mid')
+    m.style.width = '98px'
+    var t = document.getElementById('ttpd')
+    t.style.width = '98px'
+    
+}
+
+function mid(){
+    var m = document.getElementById('mid')
+    m.style.width = '380px'
+    m.style.transition = '0.4s'
+    var d = document.getElementById('debut')
+    d.style.width = '98px'
+    var s = document.getElementById('spn')
+    s.style.width = '98px'
+    var f = document.getElementById('fearless')
+    f.style.width = '98px'
+    var n = document.getElementById('nine')
+    n.style.width = '98px'
+    var r = document.getElementById('rep')
+    r.style.width = '98px'
+    var l = document.getElementById('lover')
+    l.style.width = '98px'
+    var fk = document.getElementById('red')
+    fk.style.width = '98px'
+    var e = document.getElementById('ever')
+    e.style.width = '98px'
+    var r = document.getElementById('folk')
+    r.style.width = '98px'
+    var t = document.getElementById('ttpd')
+    t.style.width = '98px'
+    
+}
+
+function ttpd(){
+    var r = document.getElementById('ttpd')
+    r.style.width = '380px'
+    r.style.transition = '0.4s'
+    var d = document.getElementById('debut')
+    d.style.width = '98px'
+    var s = document.getElementById('spn')
+    s.style.width = '98px'
+    var f = document.getElementById('fearless')
+    f.style.width = '98px'
+    var n = document.getElementById('nine')
+    n.style.width = '98px'
+    var r = document.getElementById('rep')
+    r.style.width = '98px'
+    var l = document.getElementById('lover')
+    l.style.width = '98px'
+    var fk = document.getElementById('red')
+    fk.style.width = '98px'
+    var e = document.getElementById('ever')
+    e.style.width = '98px'
+    var m = document.getElementById('folk')
+    m.style.width = '98px'
+    var t = document.getElementById('mid')
+    t.style.width = '98px'
     
 }
